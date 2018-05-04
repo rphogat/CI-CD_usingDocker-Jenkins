229f27ff8f56045cb21039b2a0e3d993a4e285e1
rphogat <rphogat@github.com>
2018-05-03 22:16:47 -0400

    pipelinestarter rollback changes

--- src/main/java/com/kodcu/PipelinesStarter.java
+++ src/main/java/com/kodcu/PipelinesStarter.java
@@ -9,6 +9,27 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class PipelinesStarter {
+
+    public static void main(String[] args){
+        SpringApplication.run(PipelinesStarter.class, args);
+    }
+}
+
+
+
+
+
+
+/*package com.kodcu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 @SpringApplication
 @ComponentScan(com.kodcu)
 public class PipelinesStarter {
@@ -17,3 +38,9 @@ public class PipelinesStarter {
         SpringApplication.run(PipelinesStarter.class, args);
     }
 }
+
+/*
+ * Created by hakdogan on 23/11/2017
+ */
+
+
