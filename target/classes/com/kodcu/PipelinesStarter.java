be8cd4e567edd06f0119b421c584f501dec874ec
rphogat <rphogat@github.com>
2018-05-05 16:30:47 -0400

    target

---
+++ target/classes/com/kodcu/PipelinesStarter.java
@@ -0,0 +1,42 @@
+package com.kodcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class PipelinesStarter {
+
+    public static void main(String[] args){
+        SpringApplication.run(PipelinesStarter.class, args);
+    }
+}
+
+
+
+
+
+
+/*package com.kodcu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringApplication
+@ComponentScan(com.kodcu)
+public class PipelinesStarter {
+
+    public static void main(String[] args){
+        SpringApplication.run(PipelinesStarter.class, args);
+    }
+}
+
+/*
+ * Created by hakdogan on 23/11/2017
+ */
+
+
